cc19290e42a502a6e61f983337547d05101919e5
Haylin Moore <me@haylinmoore.com>
2025-09-06 16:39:08 +0200

    python3Packages.meshcore: init at 2.1.5

---
+++ pkgs/development/python-modules/meshcore/default.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+
+  # build-system
+  setuptools,
+  hatchling,
+
+  # dependencies
+  bleak,
+  pycayennelpp,
+  pyserial-asyncio,
+}:
+
+buildPythonPackage rec {
+  pname = "meshcore";
+  version = "2.1.5";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-gXofzLmydfN0Shrrlbwk+6Rr2kARPqypP6+43rHZOJE=";
+  };
+
+  build-system = [
+    hatchling
+    setuptools
+  ];
+
+  dependencies = [
+    bleak
+    pycayennelpp
+    pyserial-asyncio
+  ];
+
+  pythonImportsCheck = [ "meshcore" ];
+
+  meta = with lib; {
+    description = "Python library for communicating with meshcore companion radios";
+    homepage = "https://github.com/meshcore-dev/meshcore_py";
+    license = licenses.mit;
+    maintainers = [ maintainers.haylin ];
+  };
+}
